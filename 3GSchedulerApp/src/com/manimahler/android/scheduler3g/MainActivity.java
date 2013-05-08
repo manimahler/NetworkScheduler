3ab2cc74b257ef2e2b060ea61542fbc64dc8539d
manimahler <maeni@gmx.net>
2013-05-09 00:39:56 +0200

    Remove unnecessary stuff

--- 3GSchedulerApp/src/com/manimahler/android/scheduler3g/MainActivity.java
+++ 3GSchedulerApp/src/com/manimahler/android/scheduler3g/MainActivity.java
@@ -8,9 +8,7 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
 import android.text.format.DateFormat;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TimePicker;
 import android.widget.TimePicker.OnTimeChangedListener;
@@ -24,22 +22,8 @@ import android.widget.ToggleButton;
  */
 public class MainActivity extends Activity {
 	private static final String END_TIME = "EndTime";
-
 	private static final String START_TIME = "StartTime";
-
 	private static final String SCHEDULING_ENABLED = "SchedulingEnabled";
-
-	/**
-	 * Whether or not the system UI should be auto-hidden after
-	 * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-	 */
-	private static final boolean AUTO_HIDE = false;
-
-	/**
-	 * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-	 * user interaction before hiding the system UI.
-	 */
-	private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
 
 	private boolean mSchedulingEnabled;
 
@@ -108,16 +92,6 @@ public class MainActivity extends Activity {
 		picker.setCurrentHour(calendar.get(Calendar.HOUR_OF_DAY));
 		picker.setCurrentMinute(calendar.get(Calendar.MINUTE));
 	}
-
-	@Override
-	protected void onPostCreate(Bundle savedInstanceState) {
-		super.onPostCreate(savedInstanceState);
-
-		// Trigger the initial hide() shortly after the activity has been
-		// created, to briefly hint to the user that UI controls
-		// are available.
-		delayedHide(100);
-	}
 	
 	
 	@Override
@@ -172,37 +146,6 @@ public class MainActivity extends Activity {
 		endPicker.clearFocus();
 	}
 
-	/**
-	 * Touch listener to use for in-layout UI controls to delay hiding the
-	 * system UI. This is to prevent the jarring behavior of controls going away
-	 * while interacting with activity UI.
-	 */
-	View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-		@Override
-		public boolean onTouch(View view, MotionEvent motionEvent) {
-			if (AUTO_HIDE) {
-				delayedHide(AUTO_HIDE_DELAY_MILLIS);
-			}
-			return false;
-		}
-	};
-
-	Handler mHideHandler = new Handler();
-	Runnable mHideRunnable = new Runnable() {
-		@Override
-		public void run() {
-			//mSystemUiHider.hide();
-		}
-	};
-
-	/**
-	 * Schedules a call to hide() in [delay] milliseconds, canceling any
-	 * previously scheduled calls.
-	 */
-	private void delayedHide(int delayMillis) {
-		mHideHandler.removeCallbacks(mHideRunnable);
-		mHideHandler.postDelayed(mHideRunnable, delayMillis);
-	}
 
 	private long getNextTimeMillisFromPicker(TimePicker timePicker) {
 		
