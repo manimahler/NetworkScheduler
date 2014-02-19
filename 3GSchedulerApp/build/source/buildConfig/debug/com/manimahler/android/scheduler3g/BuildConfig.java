f8ec477ea915c9856a58aa497d98a6751e14017b
manimahler <maeni@gmx.net>
2014-02-20 00:09:58 +0100

    Add gradle stuff

---
+++ 3GSchedulerApp/build/source/buildConfig/debug/com/manimahler/android/scheduler3g/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.manimahler.android.scheduler3g;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+
+}
