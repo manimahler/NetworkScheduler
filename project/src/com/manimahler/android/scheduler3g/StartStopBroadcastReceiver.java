5a29729a4000edebc2cc63007ce5a605d9d5c350
Mani Mahler <maenimahler@gmail.com>
2015-05-26 22:50:57 +0200

    Clean up

--- project/src/com/manimahler/android/scheduler3g/StartStopBroadcastReceiver.java
+++ project/src/com/manimahler/android/scheduler3g/StartStopBroadcastReceiver.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
 
 public class StartStopBroadcastReceiver extends BroadcastReceiver {
 
