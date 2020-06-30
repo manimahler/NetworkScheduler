cb2ef4bacfd78d7dc84b5a2c6e4c75ee6a788c4e
Mani Mahler <maenimahler@gmail.com>
2020-06-30 23:16:29 +0200

    Add snack bar with battery optimization hint

--- NetworkSchedulerApp/app/src/main/java/com/manimahler/android/scheduler3g/MainActivity.java
+++ NetworkSchedulerApp/app/src/main/java/com/manimahler/android/scheduler3g/MainActivity.java
@@ -16,6 +16,9 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.provider.Settings;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
@@ -38,6 +41,7 @@ import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -159,6 +163,38 @@ public class MainActivity extends AppCompatActivity implements
 			// must be saved before activating, otherwise it has ID -1 which is
 			// illegal!
 			saveSettings();
+
+
+
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+				Intent intent = new Intent();
+				String packageName = getPackageName();
+				PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+
+				if (!pm.isIgnoringBatteryOptimizations(packageName))
+				{
+					View contextView = findViewById(R.id.mainview);
+
+					Snackbar snackbar = Snackbar.make(contextView, R.string.snack_whitelist_from_optimizations, Snackbar.LENGTH_INDEFINITE);
+
+					View snackbarView = snackbar.getView();
+					TextView snackTextView = (TextView) snackbarView.findViewById(R.id.snackbar_text);
+
+					snackTextView.setMaxLines(4);
+
+					snackbar.show();
+				}
+
+				// These other approaches apparently result in trouble from google:
+//				if (!pm.isIgnoringBatteryOptimizations(packageName))
+//					intent.setAction(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS);
+//				else {
+//					intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+//					intent.setData(Uri.parse("package:" + packageName));
+//				}
+//
+//				startActivity(intent);
+			}
 
 			if (period.is_scheduleStart() && period.is_scheduleStop()
 					&& period.is_schedulingEnabled() && period.isActiveNow()) {
