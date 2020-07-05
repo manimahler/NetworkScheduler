710a667390fabf594ad01dcc6bfa114abb1f5beb
Mani Mahler <maenimahler@gmail.com>
2020-07-05 17:52:39 +0200

    Remove unnecessary (and possibly error-inducing) import

--- NetworkSchedulerApp/app/src/main/java/com/manimahler/android/scheduler3g/MainActivity.java
+++ NetworkSchedulerApp/app/src/main/java/com/manimahler/android/scheduler3g/MainActivity.java
@@ -9,7 +9,6 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.PowerManager;
-import android.provider.Settings;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
